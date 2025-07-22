5cb0b7f732e2d0d5eb653858cab9a3a1bd4aa70a
qusai438 <qsay634@gmail.com>
2025-07-22 11:45:41 +0200

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,6 +1,11 @@
 import os
 
-if os.environ.get("RENDER") == "true":
+USE_MOCK = os.getenv("MOCK_MODE", "false").lower() == "true"
+IS_RENDER = os.getenv("RENDER", "false").lower() == "true"
+
+if USE_MOCK:
+    from .mock_config import MockConfig as Config
+elif IS_RENDER:
     from .render_config import RenderConfig as Config
 else:
     from .local_config import LocalConfig as Config

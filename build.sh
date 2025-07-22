01d974e8475ac6e5836b812ae8ef27372fcb9def
qusai438 <qsay634@gmail.com>
2025-07-22 14:31:17 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
-
-# Ensure pip is up to date
-pip install --upgrade pip
-
-# Install dependencies
 pip install -r requirements.txt
-
-# Export mock mode ON if no API keys found (optional)
-export MOCK_MODE=True

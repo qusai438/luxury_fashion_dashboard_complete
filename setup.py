8f3b9576cca64a44883accf64e6f31bb4e93c854
qusai438 <qsay634@gmail.com>
2025-07-22 11:58:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="luxury_fashion_dashboard",
-    version="1.0.0",
+    name='luxury_fashion_dashboard',
+    version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        # نتركها فارغة لأن Render سيقرأ من requirements.txt
-    ],
+    install_requires=[],
 )
